b5ec293c62cfc22a524a55276a028bb1bdea5d05
ashishsah1000 <ashishsah1000@gamil.com>
2022-02-20 00:45:03 +0530

    sorted out the post method problem

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,13 +1,8 @@
 const Product = require("../models/product");
 
 exports.newProduct = async (req, res, next) => {
-  console.log("isnide new product");
-
-  //   try {
   const product = await Product.create(req.body);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
+
   res.status(201).json({
     success: true,
     product,
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -5,11 +5,6 @@ const router = express.Router();
 const { newProduct, getProducts } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
-router.route("/products/new").post((req, res) => {
-  console.log("post req detected");
-  console.log(req.body);
-  res.send("post request is working fine");
-  newProduct(req, res);
-});
+router.route("/products/new").post(newProduct);
 
 module.exports = router;
